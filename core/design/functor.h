c08a0897d51b5b0a9ee9aad4b46ca3e9d6580997
liuchaoxx <912711016@qq.com>
2015-12-14 15:53:04 +0800

    ,,

--- core/design/functor.h
+++ core/design/functor.h
@@ -49,10 +49,30 @@ public:
 	typedef typename TypeAt<TList, 0>::Result Param1;
 	typedef typename TypeAt<TList, 1>::Result Param2;
 
-	Functor (){};
-	Functor ( const Functor& fun ) { this->spImpl_ = fun.spImpl_; };
-	explicit Functor ( std::auto_ptr<Impl> spImpl ):spImpl_ ( spImpl ) {}
-	template<typename Fun> Functor ( const Fun& fun ):spImpl_ ( new FunctorHandler<Functor, Fun> ( fun ) ) {}
+	Functor ()
+	{
+	}
+
+	Functor ( const Functor& fun ) 
+	{ 
+		this->spImpl_ = fun.spImpl_; 
+	}
+
+	explicit Functor ( std::auto_ptr<Impl> spImpl ):spImpl_ ( spImpl ) 
+	{
+	}
+
+	template<typename Fun>
+	Functor ( const Fun& fun )
+		:spImpl_ ( new FunctorHandler<Functor, Fun> ( fun ) ) 
+	{
+	}
+
+	template<typename PointerToObj, typename PointerToMemFun> 
+	Functor ( const PointerToObj& pObj, PointerToMemFun pMemFun ) 
+		: spImpl_ ( new MemFunctorHandler<Functor, PointerToObj, PointerToMemFun> ( pObj, pMemFun ) ) 
+	{
+	}
 
 	Functor& operator =( const Functor& fun ) 
 	{
@@ -85,11 +105,18 @@ private:
 //functor_handler
 template<class ParentFunctor, typename Fun>
 class FunctorHandler  
-	: public FunctorImpl< typename ParentFunctor::ResultType, typename ParentFunctor::ParamList >
+	: public FunctorImpl
+	<
+		typename ParentFunctor::ResultType,
+		typename ParentFunctor::ParamList
+	>
 {
 public:
 	typedef typename ParentFunctor::ResultType ResultType;
-	FunctorHandler ( const Fun& fun ): fun_( fun ) {}
+
+	FunctorHandler ( const Fun& fun ): fun_( fun ) 
+	{
+	}
 
 	FunctorHandler* Clone() const
 	{
@@ -114,6 +141,105 @@ private:
 	Fun fun_;
 };
 
+//--------------------------------------------
+//Mem functor_handler
+template<class ParentFunctor, typename PointerToObj, typename PointerToMemFun>
+class MemFunctorHandler
+	: public FunctorImpl
+	<
+		typename ParentFunctor::ResultType, 
+		typename ParentFunctor::ParamList 
+	>
+{
+public:
+	typedef typename ParentFunctor::ResultType ResultType;
+
+	MemFunctorHandler ( const PointerToObj& pObj, PointerToMemFun pMemFun )
+		: pObj_ ( pObj ), pMemFun_ ( pMemFun ) 
+	{
+	}
+
+	MemFunctorHandler * Clone ( ) const
+	{
+		return new MemFunctorHandler ( *this );
+	}
+
+	ResultType operator()()
+	{
+		return ( ( *pObj_ ).*pMemFun_ )( );
+	}
+
+	ResultType operator()( typename ParentFunctor::Param1 p1 )
+	{
+		return ( ( *pObj_ ).*pMemFun_ )( p1 );
+	}
+
+	ResultType operator()( typename ParentFunctor::Param1 p1, typename ParentFunctor::Param2 p2 )
+	{
+		return ( ( *pObj_ ).*pMemFun_ )( p1, p2 );
+	}
+private:
+	PointerToObj pObj_;
+	PointerToMemFun pMemFun_;
+};
+
+//--------------------------------------------
+//bind first
+template<class Incoming>
+class BindFirst
+	:public FunctorImpl
+	<
+		typename Incoming::ResultType,
+		typename Incoming::Arguments::Tail
+	>
+{
+public:
+	typedef Functor<typename Incoming::ResultType, typename Incoming::Arguments::Tail> Outgoing;
+	typedef typename Incoming::Arguments::Param1 Bound;
+	typedef typename Incoming::ResultType ResultType;
+
+	BindFirst ( const Incoming& fun, Bound bound )
+		: fun_ ( fun ), bound_ ( bound )
+	{
+	}
+
+	ResultType operator()
+	{
+		return fun_ ( bound )l
+	}
+
+	ResultType operator( typename Outgoing::Param1 p1 )
+	{
+		return fun_ ( bound, p1 );
+	}
+
+	ResultType operator( typename Outgoing::Param1 p1, typename Outgoing::Param1 p2 )
+	{
+		return fun_ ( bound, p1, p2 );
+	}
+private:
+	Incoming fun_;
+	Bound bound_;
+};
+
+template<typename T>
+class BindFirstTraits
+{
+private:
+	template<class U> struct BoundFunctorTraits
+	{
+		typedef NullType BoundFunctorType;
+	};
+
+	template<class U, class V> struct BoundFunctorTraits<U V::*>
+	{
+		typedef typename U V::* BoundFunctorType;
+	};
+public:
+	typedef typename BoundFunctorTraits<T>::BoundFunctorType BoundFunctorType;
+};
+
+
 using namespace std;
 
 struct TestFunctor 
@@ -134,6 +260,21 @@ const char* test_function_retvalue ( double, double )
 	return buffer;
 }
 
+
+class MyStar
+{
+public:
+	void Show ( int, int )
+	{
+		cout << "look up, how many star, my star" << endl;
+	}
+	void Hide ( int, int )
+	{
+		cout << "hi, it is time that i disappear" << endl;
+	}
+};
+
+
 void test_functor_main ()
 {
 // 	TestFunctor f;
@@ -143,7 +284,14 @@ void test_functor_main ()
 // 	Functor<void, TYPELIST_2 ( int, double )> cmdf1 ( &test_function );
 // 	cmdf1 ( 4, 4.5 );
 
-	Functor<string, TYPELIST_2 ( int, int )> cmd2 ( &test_function_retvalue );
-	cout << cmd2 ( 10, 10 ).substr ( 7 );
+// 	Functor<string, TYPELIST_2 ( int, int )> cmd2 ( &test_function_retvalue );
+// 	cout << cmd2 ( 10, 10 ).substr ( 7 );
+
+	MyStar star;
+	Functor<void, TYPELIST_2 ( int, int )> cmd1 ( &star, &MyStar::Show );
+	Functor<void, TYPELIST_2 ( int, int )> cmd2 ( &star, &MyStar::Hide );
+	cmd1 ( 10, 10 );
+	cmd2 ( 10, 10 );
 }
+
 #endif
--- core/design/traits.h
+++ core/design/traits.h
@@ -3,67 +3,219 @@
 
 #include <vector>
 #include <iostream>
+#include "null_type.h"
+#include "empty_type.h"
 
 using namespace std;
 
-class NullType;
-
-template <typename T>
-class TypeTraits
-{
-private:
-	template<class U> struct PointerTraits
-	{
-		enum { result = false };
-		typedef NullType PointeeType;  //NullType是一种用于不能被使用情况下的占位型别
-	};
-	template<class U>struct PointerTraits<U*> //是上一个PointerTraits的偏特化，是一个针对任何指针型别的特化体，如果是指针就进入这个特化体
-	{
-		enum { result = true };
-		typedef U PointeeType;
-	};
-public:
-	enum { isPointer = PointerTraits<T>::result };
-	typedef typename PointerTraits<T>::PointeeType PointeeType;
-};
-
-
-template<typename T>
-class TypeTraits_P2M
-{
-private:
-	template<class U> struct P2MTraits
-	{
-		enum { result = false };
-	};
-	template<class U, class V> struct P2MTraits<U V::*>
-	{
-		enum { result = true };
-	};
-public:
-	enum { isMemPointer = P2MTraits<T>::result };
-};
-
-
-class A{};
-class B
-{
-public:
-	A *a;
-};
-
-void test_traits ()
-{
-	
-	cout  << TypeTraits<vector<int>::iterator>::isPointer ? 1 : 2 << '\n';
-	cout  << TypeTraits<int*>::isPointer ? 1 : 2 << '\n';
-	cout  << TypeTraits<int>::isPointer ? 1 : 2 << '\n';
-	cout  << TypeTraits_P2M<A B::*>::isMemPointer ? 1 : 2 << '\n';
-}
+// template <typename T>
+// class TypeTraits
+// {
+// private:
+// 	template<class U> struct PointerTraits
+// 	{
+// 		enum { result = false };
+// 		typedef NullType PointeeType;  //NullType是一种用于不能被使用情况下的占位型别
+// 	};
+// 	template<class U>struct PointerTraits<U*> //是上一个PointerTraits的偏特化，是一个针对任何指针型别的特化体，如果是指针就进入这个特化体
+// 	{
+// 		enum { result = true };
+// 		typedef U PointeeType;
+// 	};
+// public:
+// 	enum { isPointer = PointerTraits<T>::result };
+// 	typedef typename PointerTraits<T>::PointeeType PointeeType;
+// };
+// 
+// 
+// template<typename T>
+// class TypeTraits_P2M
+// {
+// private:
+// 	template<class U> struct P2MTraits
+// 	{
+// 		enum { result = false };
+// 	};
+// 	template<class U, class V> struct P2MTraits<U V::*>
+// 	{
+// 		enum { result = true };
+// 	};
+// public:
+// 	enum { isMemPointer = P2MTraits<T>::result };
+// };
+// 
+// 
+// class A{};
+// class B
+// {
+// public:
+// 	A *a;
+// };
+// 
+// void test_traits ()
+// {
+// 	
+// 	cout  << TypeTraits<vector<int>::iterator>::isPointer ? 1 : 2 << '\n';
+// 	cout  << TypeTraits<int*>::isPointer ? 1 : 2 << '\n';
+// 	cout  << TypeTraits<int>::isPointer ? 1 : 2 << '\n';
+// 	cout  << TypeTraits_P2M<A B::*>::isMemPointer ? 1 : 2 << '\n';
+// }
 
 // int main ()
 // {
 // 	test_traits ();
 // }
+template<bool flag, typename U, typename V>
+struct TypeSelect
+{
+	typedef U Result;
+};
 
+template<typename U, typename V>
+struct TypeSelect<false, U, V>
+{
+	typedef V Result;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////  
+/**
+* 类型萃取器, copy至 Loki库
+*/
+template<typename T>
+class TypeTraits
+{
+private:
+
+	///////////////////////////////////////////////////////  
+	//提取引用的原始类型(即去掉引用类型)  
+	template<class U>
+	struct ReferenceTraits
+	{
+		enum { result = false };
+		typedef U Result;
+	};
+
+	template<class U>
+	struct ReferenceTraits<U&>
+	{
+		enum { result = true };
+		typedef U Result;
+	};
+
+	///////////////////////////////////////////////////////  
+	//指针类型  
+	template<class U>
+	struct PointerTraits
+	{
+		enum { result = false };
+		typedef NullType Result;
+	};
+
+	template<class U>
+	struct PointerTraits<U*>
+	{
+		enum { result = true };
+		typedef U Result;
+	};
+
+	template<class U>
+	struct PointerTraits<U*&>
+	{
+		enum { result = true };
+		typedef U Result;
+	};
+
+	///////////////////////////////////////////////////////  
+	//成员函数指针, gcc 下面支持有问题, 屏蔽之  
+	template<typename U>
+	struct PointerToMemberTraits
+	{
+		enum { result = false };
+	};
+
+	template<class U, class V>
+	struct PointerToMemberTraits< U V::*>
+	{
+		enum { result = true };
+	};
+
+	template<class U, class V>
+	struct PointerToMemberTraits< U V::*&>
+	{
+		enum { result = true };
+	};
+
+	///////////////////////////////////////////////////////  
+	// const  
+	template<typename U>
+	struct UnConstTraits
+	{
+		enum { result = false };
+		typedef U Result;
+	};
+	template<typename U>
+	struct UnConstTraits<const U>
+	{
+		enum { result = true };
+		typedef U Result;
+	};
+	template<typename U>
+	struct UnConstTraits<const U&>
+	{
+		enum { result = true };
+		typedef U& Result;
+	};
+
+	///////////////////////////////////////////////////////  
+	// volatile  
+	template<typename U>
+	struct UnVolatileTraits
+	{
+		enum { result = false };
+		typedef U Result;
+	};
+	template<typename U>
+	struct UnVolatileTraits<volatile U>
+	{
+		enum { result = true };
+		typedef U Result;
+	};
+	template<typename U>
+	struct UnVolatileTraits<volatile U&>
+	{
+		enum { result = true };
+		typedef U& Result;
+	};
+public:
+	//T是否是指针类型  
+	enum { isPointer = PointerTraits< T>::result };
+	//T是否是引用类型  
+	enum { isReference = ReferenceTraits<T >::result };
+	//T是否指向成员函数的指针  
+	enum { isMemberPointer = PointerToMemberTraits<T >::result };
+
+	//T是否是Const类型  
+	enum { isConst = UnConstTraits< T>::result };
+	//T是否是Volatile类型  
+	enum { isVolatile = UnVolatileTraits< T>::result };
+
+	//如果T是指针类型,则获取T的原类型, 即去掉指针类型  
+	typedef typename PointerTraits<T>::Result           PointeeType;
+	//如果T是引用类型,则获取T的原类型, 即去掉引用类型  
+	typedef typename ReferenceTraits<T>::Result         ReferencedType;
+	//如果T是Const类型,则获取T的原类型, 即去掉 Const类型  
+	typedef typename UnConstTraits<T>::Result           NonConstType;
+	//如果T是volatile类型,则获取T的原类型, 即去掉volatile类型  
+	typedef typename UnVolatileTraits<T>::Result        NonVolatileType;
+	//去掉const volatile类型  
+	typedef typename UnVolatileTraits<typename UnConstTraits<T >::Result >::Result UnqualifiedType;
+
+public:
+
+	//对于复杂类型且非空类型, 获取数据的引用类型, 即加上引用类型  
+	//typedef typename TypeSelect<isBaseType || isNullType, T, ReferencedType&>::Result ReferenceTypeEx;  
+
+	//获取数据的原类型, 消除引用的引用这种情况  
+	typedef typename TypeSelect<!isReference, T, ReferencedType&>::Result ParameterType;
+};
 #endif
